aca2a543b70d8a8a619a4b62e19879625a379dd9
VLAD <vlad@VLADs-MacBook-Air.local>
2019-03-01 19:40:48 -0600

    message

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,60 +5,23 @@ import csv
 # Set path for file
 csvpath = "Desktop/Budget_data.csv"
 
+
+revenueSum = 0
+totalcount = 0
+
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvpath)
+    csvreader = csv.reader(csvfile, delimiter= ',')
     
     #print(csvreader)
-    #csv_header = next(csvreader)
+    csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
+    for row in csvreader:
+        totalcount = totalcount + 1
+        revenueSum  = revenueSum + float(row[1])
+        average = round(revenueSum/totalcount)
     
 print ("Financial Analysis")
 print ("-------------------")
-
-monthCount = 0
-revenueSum = 0
-
-row= [ rows in csvreader]
-
-for row in csvreader:
-    count(Date)
-    
-        count += 1
-print (count)   
-
-column = 3 
-for column in csvreader:
-    minimum= min(column)
-print (minimum)
-
-   
-        
-reader_file = csv.reader(csvpath)
-value = len(list(reader_file))
-print (value)
-
-
-for column in csvreader:
-    count += 1
-
-average_change = value/count
-print (average_change)
-
-
-print ("Total Months: " + str(count))
-
-
-
-
-for column in csvreader:
-    sum += int(column[1])
-    
-print (sum)
-  
-print ("Total : " + str(sum))
-
-
-
-for column in csvreader:
-    maximum = max (column[1])
-print (maximum)
+print ("Total Months: " + str(totalcount))
+print ("Total: " + str(revenueSum))   
+print ("Average Profit: " +str(average))
