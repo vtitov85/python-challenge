3f378c8ade5e058d139d99d654113c59710f0c46
VLAD <vlad@VLADs-MacBook-Air.local>
2019-03-01 22:40:24 -0600

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,16 @@ csvpath = "Desktop/Budget_data.csv"
 
 revenueSum = 0
 totalcount = 0
+maxvalue = 0
+minvalue = 0
+maxdate = None
+mindate = None
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
+    
+    
     
     #print(csvreader)
     csv_header = next(csvreader)
@@ -19,9 +26,29 @@ with open(csvpath) as csvfile:
         totalcount = totalcount + 1
         revenueSum  = revenueSum + float(row[1])
         average = round(revenueSum/totalcount)
-    
+        
+        if maxvalue < float(row[1]): 
+            maxvalue = float (row[1])
+            maxdate = row[0]
+            
+        if minvalue > float(row[1]): 
+            minvalue = float (row[1])
+            mindate = row[0]
+             
 print ("Financial Analysis")
 print ("-------------------")
 print ("Total Months: " + str(totalcount))
 print ("Total: " + str(revenueSum))   
 print ("Average Profit: " +str(average))
+print ("Greatest Profit: " + maxdate +" "+str(maxvalue))
+print ("Greatest Losses: " + mindate +" "+str(minvalue))
+
+output_path = os.path.join("Desktop", "profit.csv")
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Criteria', 'Value'])
+    csvwriter.writerow(['Total Months', str(totalcount)])
+    csvwriter.writerow(['Total', str(revenueSum)])
+    csvwriter.writerow(['Average Profit', str(average)])
+    csvwriter.writerow(['Greatest Profit', maxdate +" "+str(maxvalue)])
+    csvwriter.writerow(['Greatest Losses', mindate +" "+str(minvalue)])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,42 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 19:44:55 2019
+
+@author: vlad
+"""
+
 import os
 import csv
 
 # Set path for file
-csvpath = "Desktop/election_data.csv"
+csvpath = "/Users/vlad/Desktop/election_data.csv"
 
+totalcount_1 = 0
+totalcount_2 = 0
+totalcount_3 = 0
+totalcount_4 = 0
 totalcount = 0
-votes = {}
-maxVotes = 0
-
-logFile=open('output.log','w')
-
-def log(text):
-    print(text)
-    if logFile:
-        logFile.write(str(text) + "\n")
+name1 = "Khan"
+name2 = "Correy"
+name3 = "Li"
+name4 = "O'Tooley"
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
     for row in csvreader:
-        totalcount += 1
-        candidate = row[2]
-        if not candidate in votes:
-            votes[candidate] = 0
-        votes[candidate] += 1
+        totalcount = totalcount + 1
 
-log("Election Results")
-log("-------------------")
-log("Total Votes: " + str(totalcount))
-log("-------------------")
-for k, v in votes.items():
-    if v > maxVotes:
-        winner = k
-        maxVotes = v        
-    log(str(k) + ": " + format(v * 100/totalcount, '.3f') + "%(" + str(v) + ")")
-log("-------------------")
-log("Winner: " + winner)
-log("-------------------")
+        
+        if row[2] == name1:
+            totalcount_1 = totalcount_1 + 1
+        
+        if row[2] == name2:
+            totalcount_2 = totalcount_2 + 1
+            
+        if row[2] == name3:
+            totalcount_3 = totalcount_3 + 1 
+            
+        if row[2] == name4:
+            totalcount_4 = totalcount_4 + 1
+              
 
-logFile.close()
+print("Election Results")
+print("-------------------")
+print("Total Votes: " + str(totalcount))
+print("-------------------")
+print(name1 + ": " + str(round(totalcount_1 / totalcount * 100)) +"% " + str(totalcount_1))
+print(name2 + ": " + str(round(totalcount_2 / totalcount * 100)) +"% " + str(totalcount_2)) 
+print(name3 + ": " + str(round(totalcount_3 / totalcount * 100)) +"% " + str(totalcount_3))
+print(name4 + ": " + str(round(totalcount_4 / totalcount * 100)) +"% " + str(totalcount_4))
+
+
+print("-------------------")
+if (totalcount_1 > totalcount_2) & (totalcount_1 > totalcount_3) & (totalcount_1 > totalcount_4):
+    print("Winner: " + name1)
+if (totalcount_2 > totalcount_1) & (totalcount_2 > totalcount_3) & (totalcount_2 > totalcount_4):
+    print("Winner: " + name2)   
+if (totalcount_3 > totalcount_1) & (totalcount_3 > totalcount_2) & (totalcount_3 > totalcount_4):
+    print("Winner: " + name3)    
+if (totalcount_4 > totalcount_1) & (totalcount_4 > totalcount_2) & (totalcount_4 > totalcount_3):
+    print("Winner: " + name4)    
+    
+print("-------------------")
+
+output_path = os.path.join("Desktop", "election.csv")
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow([name1, str(round(totalcount_1 / totalcount * 100)), str(totalcount_1)])
+    csvwriter.writerow([name2, str(round(totalcount_2 / totalcount * 100)), str(totalcount_2)])
+    csvwriter.writerow([name3, str(round(totalcount_3 / totalcount * 100)), str(totalcount_3)])
+    csvwriter.writerow([name4, str(round(totalcount_4 / totalcount * 100)), str(totalcount_4)])
+    csvwriter.writerow(["Winner: " + name1])
+
